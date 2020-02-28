9070e02462816e81988eda4cab5e50657d67a023
Romeo <romeovs@gmail.com>
2020-02-28 12:06:56 +0100

    Do not run test in action but ask for location of lcov file

--- index.js
+++ index.js
@@ -1,54 +1,65 @@
 const core = require("@actions/core");
-const fs = require("fs");
-const coverage = require("coverage-percentage");
-const { execSync } = require("child_process");
+const fs = require("fs").promises;
+const lcov = require("lcov-parse");
 const { GitHub, context } = require("@actions/github");
 
-function getPercentage() {
+async function main() {
+  const token = core.getInput("github-token");
+	const lcovFile = core.getInput("lcov-file") || "./coverage/lcov.info";
+
+	let raw = null
+	try {
+		const raw = await fs.readFile(lcovFile)
+	} catch(err) {
+    console.log(`No coverage report found at '${lcovFile}', exiting...`)
+    return
+	}
+
+	const lcov = await parse(raw)
+
+  const comment = `
+Total Coverage: \`${percentage(lcov).toFixed(2)}%\`
+`;
+
+  const client = new GitHub(token);
+  await client.issues.createComment({
+    repo: context.repo.repo,
+    owner: context.repo.owner,
+    issue_number: await PR(),
+    body: comment,
+  });
+};
+
+// Read the PR number from the GITHUB_EVENT_PATH
+async function PR() {
+	const data = await fs.readFile(process.env.GITHUB_EVENT_PATH, 'utf8')
+	const event = JSON.parse(data)
+  return event.pull_request.number
+}
+
+// Parse lcov string into lcov data
+function parse(data) {
   return new Promise(function (resolve, reject) {
-    const coveragePercentage = coverage("./coverage/lcov.info", "lcov", function (err, data) {
+		parse(data, function (err, res) {
       if (err) {
         reject(err)
         return
       }
       resolve(data)
-    })
-  })
-}
-
-const main = async () => {
-  const repoName = context.repo.repo;
-  const repoOwner = context.repo.owner;
-  const githubToken = core.getInput("github-token");
-  const testCommand = core.getInput("test-command") || "npx jest";
-
-  const githubClient = new GitHub(githubToken);
-  const prNumber = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')).pull_request.number
-  const codeCoverage = execSync(testCommand).toString();
-
-  let coveragePercentage = null
-  try {
-    coveragePercentage = (await getPercentage()).toFixed(2)
-  } catch (err) {
-    console.log('No coverage report found, exiting...')
-    return
-  }
-
-  const commentBody = `<div>
-    <p>Total Coverage: <code>${coveragePercentage}%</code></p>
-      <details><summary>Coverage report</summary>
-        <p>
-          <pre>${codeCoverage}</pre>
-        </p>
-      </details>
-    </div>`;
-
-  await githubClient.issues.createComment({
-    repo: repoName,
-    owner: repoOwner,
-    body: commentBody,
-    issue_number: prNumber,
-  });
+		});
+	});
 };
+
+// Get the total coverage percentage from the lcov data.
+function percentage(lcov) {
+	let hit = 0;
+	let found = 0;
+	for (const entry of lcov) {
+		hit += entry.lines.hit;
+		found += entry.lines.found;
+	}
+
+	return (hit / found) * 100
+}
 
 main().catch(err => core.setFailed(err.message));
