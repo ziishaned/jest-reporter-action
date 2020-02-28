212fe3e07df19d9407fab25acbe1c7d7a027e9cf
Romeo <romeovs@gmail.com>
2020-02-28 12:08:01 +0100

    Set up editorconfig

--- index.js
+++ index.js
@@ -4,48 +4,48 @@ const lcov = require("lcov-parse");
 const { GitHub, context } = require("@actions/github");
 
 async function main() {
-  const token = core.getInput("github-token");
+	const token = core.getInput("github-token");
 	const lcovFile = core.getInput("lcov-file") || "./coverage/lcov.info";
 
 	let raw = null
 	try {
 		const raw = await fs.readFile(lcovFile)
 	} catch(err) {
-    console.log(`No coverage report found at '${lcovFile}', exiting...`)
-    return
+		console.log(`No coverage report found at '${lcovFile}', exiting...`)
+		return
 	}
 
 	const lcov = await parse(raw)
 
-  const comment = `
+	const comment = `
 Total Coverage: \`${percentage(lcov).toFixed(2)}%\`
 `;
 
-  const client = new GitHub(token);
-  await client.issues.createComment({
-    repo: context.repo.repo,
-    owner: context.repo.owner,
-    issue_number: await PR(),
-    body: comment,
-  });
+	const client = new GitHub(token);
+	await client.issues.createComment({
+		repo: context.repo.repo,
+		owner: context.repo.owner,
+		issue_number: await PR(),
+		body: comment,
+	});
 };
 
 // Read the PR number from the GITHUB_EVENT_PATH
 async function PR() {
 	const data = await fs.readFile(process.env.GITHUB_EVENT_PATH, 'utf8')
 	const event = JSON.parse(data)
-  return event.pull_request.number
+	return event.pull_request.number
 }
 
 // Parse lcov string into lcov data
 function parse(data) {
-  return new Promise(function (resolve, reject) {
+	return new Promise(function (resolve, reject) {
 		parse(data, function (err, res) {
-      if (err) {
-        reject(err)
-        return
-      }
-      resolve(data)
+			if (err) {
+				reject(err)
+				return
+			}
+			resolve(data)
 		});
 	});
 };
