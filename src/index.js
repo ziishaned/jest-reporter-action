2517692c775725465cfd712925c3816b11f44ea3
Romeo <romeovs@gmail.com>
2020-02-28 12:28:48 +0100

    Remove semicolons

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const core = require("@actions/core");
-const fs = require("fs").promises;
-const lcov = require("lcov-parse");
-const { GitHub, context } = require("@actions/github");
+const core = require("@actions/core")
+const fs = require("fs").promises
+const lcov = require("lcov-parse")
+const { GitHub, context } = require("@actions/github")
 
 async function main() {
-	const token = core.getInput("github-token");
-	const lcovFile = core.getInput("lcov-file") || "./coverage/lcov.info";
+	const token = core.getInput("github-token")
+	const lcovFile = core.getInput("lcov-file") || "./coverage/lcov.info"
 
 	let raw = null
 	try {
@@ -19,16 +19,16 @@ async function main() {
 
 	const comment = `
 Total Coverage: \`${percentage(lcov).toFixed(2)}%\`
-`;
+`
 
-	const client = new GitHub(token);
+	const client = new GitHub(token)
 	await client.issues.createComment({
 		repo: context.repo.repo,
 		owner: context.repo.owner,
 		issue_number: await PR(),
 		body: comment,
-	});
-};
+	})
+}
 
 // Read the PR number from the GITHUB_EVENT_PATH
 async function PR() {
@@ -46,20 +46,20 @@ function parse(data) {
 				return
 			}
 			resolve(data)
-		});
-	});
-};
+		})
+	})
+}
 
 // Get the total coverage percentage from the lcov data.
 function percentage(lcov) {
-	let hit = 0;
-	let found = 0;
+	let hit = 0
+	let found = 0
 	for (const entry of lcov) {
-		hit += entry.lines.hit;
-		found += entry.lines.found;
+		hit += entry.lines.hit
+		found += entry.lines.found
 	}
 
 	return (hit / found) * 100
 }
 
-main().catch(err => core.setFailed(err.message));
+main().catch(err => core.setFailed(err.message))
