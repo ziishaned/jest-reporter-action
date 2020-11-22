47b33d59858ce131d573d6dc61a29724ceb32d38
Rich Burdon <rich.burdon@gmail.com>
2020-11-22 14:56:35 -0500

    Test

--- src/index.js
+++ src/index.js
@@ -9,29 +9,29 @@ const main = async () => {
   const testCommand = core.getInput("test-command") || "npx jest";
 
   const githubClient = new GitHub(githubToken);
-  const commitPRs = await githubClient.repos.listPullRequestsAssociatedWithCommit(
-    {
-      repo: repoName,
-      owner: repoOwner,
-      commit_sha: context.sha
-    }
-  );
+  const commitPRs = await githubClient.repos.listPullRequestsAssociatedWithCommit({
+    repo: repoName,
+    owner: repoOwner,
+    commit_sha: context.sha
+  });
+
   const pr = commitPRs.data[0];
-  if(!pr) return; // This is not a PR, nothing to comment
+  if (!pr) return; // This is not a PR, nothing to comment.
   const prNumber = pr.number;
 
   const codeCoverage = execSync(testCommand).toString();
   let coveragePercentage = execSync(
-    "npx coverage-percentage ./coverage/lcov.info --lcov"
+    'npx coverage-percentage ./coverage/lcov.info --lcov'
   ).toString();
   coveragePercentage = parseFloat(coveragePercentage).toFixed(2);
 
-  const commentBody = `<p>Total Coverage: <code>${coveragePercentage}</code></p>
-<details><summary>Coverage report</summary>
-<p>
-<pre>${codeCoverage}</pre>
-</p>
-</details>`;
+  const commentBody = `
+<p>Total Coverage: <code>${coveragePercentage}</code></p>
+<details>
+<summary>Coverage report</summary>
+<p><pre>${codeCoverage}</pre></p>
+</details>
+`;
 
   await githubClient.issues.createComment({
     repo: repoName,
