e96a2c066f1b89de061757c3a7455f7e8eb19331
GY Noh <gynoh@getmiso.com>
2021-02-27 22:49:55 +0900

    Leave comment by input

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-const core = require("@actions/core");
-const { execSync } = require("child_process");
-const { GitHub, context } = require("@actions/github");
+const core = require('@actions/core');
+const { GitHub, context } = require('@actions/github');
 
 const main = async () => {
-  const repoName = context.repo.repo;
-  const repoOwner = context.repo.owner;
-  const githubToken = core.getInput("github-token");
-  const testCommand = core.getInput("test-command") || "npx jest";
+  const {repo, owner} = context.repo;
+  const githubToken = core.getInput('github-token');
+  const coverage = core.getInput('coverage');
 
   const githubClient = new GitHub(githubToken);
-  const commitPRs = await githubClient.repos.listPullRequestsAssociatedWithCommit(
-    {
-      repo: repoName,
-      owner: repoOwner,
-      commit_sha: context.sha
-    }
-  );
-  const prNumber = commitPRs.data[0].number;
-
-  const codeCoverage = execSync(testCommand).toString();
-  let coveragePercentage = execSync(
-    "npx coverage-percentage ./coverage/lcov.info --lcov"
-  ).toString();
-  coveragePercentage = parseFloat(coveragePercentage).toFixed(2);
-
-  const commentBody = `<p>Total Coverage: <code>${coveragePercentage}</code></p>
-<details><summary>Coverage report</summary>
-<p>
-<pre>${codeCoverage}</pre>
-</p>
-</details>`;
-
-  await githubClient.issues.createComment({
-    repo: repoName,
-    owner: repoOwner,
-    body: commentBody,
-    issue_number: prNumber,
+  const commitPRs = await githubClient.repos.listPullRequestsAssociatedWithCommit({
+    repo, owner, commit_sha: context.sha
   });
+  const issue_number = commitPRs.data[0].number;
+  const percentage = JSON.parse(coverage).result.covered_percent;
+  const body = `<p>Total Coverage: <code>${percentage}</code>`;
+
+  await githubClient.issues.createComment({repo, owner, body, issue_number});
 };
 
 main().catch(err => core.setFailed(err.message));
